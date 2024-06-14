f8959b69a3b62b7dffeacaec74d5adb452e69804
Cristian <cristian.bolarin@gmail.com>
2024-06-14 20:58:00 +0200

    subida vista crear actualizar

---
+++ Solution/Client/Services/AlojamientosService.cs
@@ -0,0 +1,10 @@
+﻿namespace Client.Services
+{
+    public class AlojamientosService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string apiUri = "https://localhost:7123/api/";
+
+
+    }
+}
